f13a7c144ad7155f6967aa56c8c929f70d7f3beb
=subhrangshu <=subhrangshus@gmail.com>
2020-09-02 12:54:31 +0000

    changes for us-west-2 deployment

--- vpc.tf
+++ vpc.tf
@@ -1,17 +1,17 @@
 variable "region" {
-  default     = "us-east-1"
+  default     = "us-west-2"
   description = "AWS region"
 }
 
 provider "aws" {
   version = ">= 2.28.1"
-  region  = "us-east-1"
+  region  = "us-west-2"
 }
 
 data "aws_availability_zones" "available" {}
 
 locals {
-  cluster_name = "training-eks-${random_string.suffix.result}"
+  cluster_name = "terra-eks-${random_string.suffix.result}"
 }
 
 resource "random_string" "suffix" {
@@ -23,7 +23,7 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "2.6.0"
 
-  name                 = "training-vpc"
+  name                 = "terra-vpc"
   cidr                 = "10.0.0.0/16"
   azs                  = data.aws_availability_zones.available.names
   private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
